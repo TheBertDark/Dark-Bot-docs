1d35a8ab7bc93e7d4ae59a8bd8f2407e3f466c45
Dark-Bot Documentation <estbd11@gmail.com>
2025-06-16 23:29:36 -0600

    Fix CSS import path and add missing custom-style.css file

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,7 +3,7 @@ import { h } from "vue";
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 import "./custom.css";
-import "../../../custom-style.css";
+import "../../custom-style.css";
 
 export default {
   ...DefaultTheme,
